e9e8118cd279f1cce852c8c82824db5b38094fbb
CosLaso <88412976+CosLaso@users.noreply.github.com>
2022-05-16 03:10:18 +1000

    Advances in IBVS

--- Simulation/Main.m
+++ Simulation/Main.m
@@ -186,153 +186,144 @@ hold on
 
 %% IBVS (Image Based Visual Servoing) [Lab 10]
 
-% % Using a UR10 - basic sutff
-% r = UR10(); 
-% qt =  [1.6; -1; -1.2; -0.5; 0; 0];
-% focal = 0.08;
-% pixel = 10e-5;
-% resolution = [1024 1024];
-% centre = [512 512];
-% pStar =  [700 300 300 700; 300 300 700 700]; 
-% P=[2,2,2,2; -0.4,0.4,0.4,-0.4; 1.4,1.4,0.6,0.6]; 
-% 
-% q = qt';
-% cam = CentralCamera('focal', focal, 'pixel', pixel, 'resolution', resolution, 'centre', centre); 
-% Tc0 = r.model.fkine(q);
-% cam.T = Tc0;
-% uv = cam.plot(P);
-% e = pStar - uv
-% round(e)
-% 
-% % Using Lab 8 (without plotting) - 1.1 Definitions
-% % Create image target (points in the image plane) 
-% pStar = [662 362 362 662; 362 362 662 662];
-% 
-% %Create 3D points
-% P=[1.8,1.8,1.8,1.8;
-% -0.25,0.25,0.25,-0.25;
-%  1.25,1.25,0.75,0.75];
-% 
-% % Make a UR10
-% Dobot = Dobot(false);          
-% 
-% %Initial pose
-% q0 = [pi/2; 0; 0; 0];
-% 
-% % Add the camera
-% cam = CentralCamera('focal', 0.08, 'pixel', 10e-5, ...
-% 'resolution', [1024 1024], 'centre', [512 512],'name', 'UR10camera');
-% 
-% % frame rate
-% fps = 25;
-% 
-% % Define values
-% % Gain of the controler
-% lambda = 0.6;
-% 
-% % Depth of the IBVS
-% depth = mean (P(1,:));
-% 
-% % 1.2 Initialise Simulation (Display in 3D)
-% % Display UR10
-% Tc0= Dobot.model.fkine(q0);
-% Dobot.model.animate(q0');
-% drawnow();
-% 
-% % Plot camera and points
-% cam.T = Tc0;
-% 
-% % Display points in 3D and the camera
-% cam.plot_camera('Tcam',Tc0, 'label','scale',0.15);
-% plot_sphere(P, 0.05, 'b')
-% lighting gouraud
-% light
-% 
-% % 1.3 Initialise Simulation (Display in Image view)
-% % Project points to the image
-% p = cam.plot(P, 'Tcam', Tc0);
-% 
-% % Camera view and plotting
-% cam.clf()
-% cam.plot(pStar, '*'); % create the camera view
-% cam.hold(true);
-% cam.plot(P, 'Tcam', Tc0, 'o'); % create the camera view
-% pause(2)
-% cam.hold(true);
-% cam.plot(P);    % show initial view
-% 
-% % Initialise display arrays
-% vel_p = [];
-% uv_p = [];
-% history = [];
+% Definitions
+% Create image target (points in the image plane) 
+pStar = [662 362 362 662; 362 362 662 662];
 
-% % 1.4 Loop
-% % Loop of the visual servoing
-% ksteps = 0;
-%  while true
-%         ksteps = ksteps + 1;
-%         % compute the view of the camera
-%         uv = cam.plot(P);
-%         % compute image plane error as a column
-%         e = pStar-uv;   % feature error
-%         e = e(:);
-%         Zest = [];
-%         % compute the Jacobian
-%         if isempty(depth)
-%             % exact depth from simulation (not possible in practice)
-%             pt = homtrans(inv(Tcam), P);
-%             J = cam.visjac_p(uv, pt(3,:) );
-%         elseif ~isempty(Zest)
-%             J = cam.visjac_p(uv, Zest);
-%         else
-%             J = cam.visjac_p(uv, depth );
-%         end
-%         % compute the velocity of camera in camera frame
-%         try
-%             v = lambda * pinv(J) * e;
-%         catch
-%             status = -1;
-%             return
-%         end
-%         fprintf('v: %.3f %.3f %.3f %.3f %.3f %.3f\n', v);
-%         %compute robot's Jacobian and inverse
-%         J2 = Dobot.model.jacobn(q0);
-%         Jinv = pinv(J2);
-%         % get joint velocities
-%         qp = Jinv*v;
-%          %Maximum angular velocity cannot exceed 180 degrees/s
-%          ind=find(qp>pi);
-%          if ~isempty(ind)
-%              qp(ind)=pi;
-%          end
-%          ind=find(qp<-pi);
-%          if ~isempty(ind)
-%              qp(ind)=-pi;
-%          end
-%         %Update joints 
-%         q = q0 + (1/fps)*qp;
-%         Dobot.model.animate(q');
-%         %Get camera location
-%         Tc = Dobot.model.fkine(q);
-%         cam.T = Tc;
-%         drawnow
-%         % update the history variables
-%         hist.uv = uv(:);
-%         vel = v;
-%         hist.vel = vel;
-%         hist.e = e;
-%         hist.en = norm(e);
-%         hist.jcond = cond(J);
-%         hist.Tcam = Tc;
-%         hist.vel_p = vel;
-%         hist.uv_p = uv;
-%         hist.qp = qp;
-%         hist.q = q;
-%         history = [history hist];
-%          pause(1/fps)
-%         if ~isempty(200) && (ksteps > 200)
-%             break;
-%         end
-%         %update current joint position
-%         q0 = q;
-%  end %loop finishes
+%Create 3D points
+P = [1.8,1.8,1.8,1.8; -0.25,0.25,0.25,-0.25; 1.25,1.25,0.75,0.75];
+
+% Make the Dobot
+% Dobot = Dobot();
+DobotWS = DobotWithoutSuction();          
+
+%Initial pose
+q0 = [pi/2, -pi/6, 0];
+
+% Add the camera
+cam = CentralCamera('focal', 0.08, 'pixel', 10e-5, 'resolution', [1024 1024], 'centre', [512 512],'name', 'UR10camera');
+
+% frame rate
+fps = 25;
+
+% Define values
+% Gain of the controler
+lambda = 0.6;
+
+% Depth of the IBVS
+depth = mean (P(1,:));
+
+% Initialise Simulation (Display in 3D)
+% Display Dobot
+Tc0= DobotWS.model.fkine(q0);
+DobotWS.model.animate(q0);
+drawnow();
+
+% Plot camera and points
+cam.T = Tc0;
+
+% Display points in 3D and the camera
+cam.plot_camera('Tcam',Tc0, 'label','scale',0.075);
+plot_sphere(P, 0.025, 'b')
+lighting gouraud
+light
+
+% Initialise Simulation (Display in Image view)
+% Project points to the image
+p = cam.plot(P, 'Tcam', Tc0);
+
+% Camera view and plotting
+cam.clf()
+cam.plot(pStar, '*'); % create the camera view
+cam.hold(true);
+cam.plot(P, 'Tcam', Tc0, 'o'); % create the camera view
+pause(2)
+cam.hold(true);
+cam.plot(P);    % show initial view
+
+% Initialise display arrays
+vel_p = [];
+uv_p = [];
+history = [];
+
+% Loop of the visual servoing
+ksteps = 0;
+while true
+    ksteps = ksteps + 1;
+
+    % Compute the view of the camera
+    uv = cam.plot(P);
+
+    % Compute image plane error as a column
+    e = pStar-uv;   % feature error
+    e = e(:);
+    Zest = [];
+
+    % Compute the Jacobian
+    if isempty(depth)
+        % Exact depth from simulation (not possible in practice)
+        pt = homtrans(inv(Tcam), P);
+        J = cam.visjac_p(uv, pt(3,:) );
+    elseif ~isempty(Zest)
+        J = cam.visjac_p(uv, Zest);
+    else
+        J = cam.visjac_p(uv, depth );
+    end
+
+    % Compute the velocity of camera in camera frame
+    try
+        v = lambda * pinv(J) * e;
+    catch
+        status = -1;
+        return
+    end
+    fprintf('v: %.3f %.3f %.3f %.3f %.3f %.3f\n', v);
+
+    % Compute robot's Jacobian and inverse
+    J2 = DobotWS.model.jacobn(q0);
+    Jinv = pinv(J2);
+
+    % Get joint velocities
+    qp = Jinv*v;
+
+     % Maximum angular velocity cannot exceed 180 degrees/s
+     ind=find(qp>pi);
+     if ~isempty(ind)
+         qp(ind)=pi;
+     end
+     ind=find(qp<-pi);
+     if ~isempty(ind)
+         qp(ind)=-pi;
+     end
+
+    % Update joints 
+    q = q0 + (1/fps)*qp;
+    DobotWS.model.animate(q);
+
+    % Get camera location
+%     Tc = DobotWS.model.fkine(q);
+    Tc = DobotWS.model.fkine(DobotWS.model.getpos());
+    cam.T = Tc;
+    drawnow();
+
+    % Update the history variables
+    hist.uv = uv(:);
+    vel = v;
+    hist.vel = vel;
+    hist.e = e;
+    hist.en = norm(e);
+    hist.jcond = cond(J);
+    hist.Tcam = Tc;
+    hist.vel_p = vel;
+    hist.uv_p = uv;
+    hist.qp = qp;
+    hist.q = q;
+    history = [history hist];
+     pause(1/fps)
+    if ~isempty(200) && (ksteps > 200)
+        break;
+    end
+
+    % Update current joint position
+    q0 = q;
+end
