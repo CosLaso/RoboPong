f8d09a8ceb930847b6be4887b1245b8c3099b933
CosLaso <88412976+CosLaso@users.noreply.github.com>
2022-05-16 17:28:45 +1000

    Final (long pauses)

--- Real Robot/Integrated Code/Holistic_Code.m
+++ Real Robot/Integrated Code/Holistic_Code.m
@@ -5,7 +5,7 @@ close all;
 clear all;
 clc;
 
-rosshutdown;        % Call this at the start just in case
+rosshutdown;        % Need to call this at the start
 rosinit;            % Initialise connection
 
 %% Initialise the Camera
@@ -13,33 +13,29 @@ rosinit;            % Initialise connection
 sub = rossubscriber("camera/color/image_raw");
 [msg2] = receive(sub);
 image = readImage(msg2);
-fprintf('Camera is initialised\n');
+fprintf('\nCamera is initialised\n');
 
 %% Find Centriods of Blocks
-    
-% Centre of Red
+
+% Centres of coloured blocks
 extracted_red = extract_red(image);                     % Outputs binary (0,1) same size array with red as 1
-red_centre = calculate_centroid(extracted_red);         % Outputs pixel coordinates of red cube centre
+red_centre = calculateCentroid(extracted_red);          % Outputs pixel coordinates of red cube centre
 % figure, imshow(extracted_red);                          % Show the extracted RED (RED pixels are white, every other pixel is black)
 
-% Centre of Green
 extracted_green = extract_green(image);
-green_centre = calculate_centroid(extracted_green);     % Outputs pixel coordinates of green cube centre
+green_centre = calculateCentroid(extracted_green);      % Outputs pixel coordinates of green cube centre
 % figure, imshow(extracted_green);
 
-% Centre of Yellow
 extracted_yellow = extract_yellow(image);
-yellow_centre = calculate_centroid(extracted_yellow);   %  Outputs pixel coordinates of yellow cube centre
+yellow_centre = calculateCentroid(extracted_yellow);    %  Outputs pixel coordinates of yellow cube centre
 % figure, imshow(extracted_yellow);
 
-% Centre of Blue
 extracted_blue = extract_blue(image);
-blue_centre = calculate_centroid(extracted_blue);   %  Outputs pixel coordinates of blue cube centre
+blue_centre = calculateCentroid(extracted_blue);        %  Outputs pixel coordinates of blue cube centre
 % figure, imshow(extracted_blue);
 
-% Centre of Orange
 extracted_orange = extract_orange(image);
-orange_centre = calculate_centroid(extracted_orange);   %  Outputs pixel coordinates of blue cube centre
+orange_centre = calculateCentroid(extracted_orange);    %  Outputs pixel coordinates of blue cube centre
 % figure, imshow(extracted_orange);
 
 % Plot results
@@ -64,84 +60,55 @@ text(orange_centre.Centroid(1) + 10, orange_centre.Centroid(2),'ORANGE','FontSiz
 %% Calculate 3D Coordinates in Camera Frame
 
 % Camera Parameters
- focalLength = (917.6252+915.7077)/2;
- PPx = 636.2969;
- PPy = 351.4231;
+focalLength = (917.6252+915.7077)/2;
+PPx = 636.2969;
+PPy = 351.4231;
+ 
+% Depth (z value) from Camera (conversion of mm to m)
+zCameraToBlock = 400/1000;  
 
-% Move Origin to Principle Point:
-Red_X_L = red_centre.Centroid(1,1) - PPx;
-Red_Y_L = red_centre.Centroid(1,2) - PPy;
-
-Green_X_L = green_centre.Centroid(1,1) - PPx;
-Green_Y_L = green_centre.Centroid(1,2) - PPy;
-
-Yellow_X_L = yellow_centre.Centroid(1,1) - PPx;
-Yellow_Y_L = yellow_centre.Centroid(1,2) - PPy;
-
-Blue_X_L = blue_centre.Centroid(1,1) - PPx;
-Blue_Y_L = blue_centre.Centroid(1,2) - PPy;
-
-Orange_X_L = orange_centre.Centroid(1,1) - PPx;
-Orange_Y_L = orange_centre.Centroid(1,2) - PPy;
-        
-% Collect Depth (Z value) information from Camera (conversion of mm to m)
-Z_Cam_To_Block = 557/1000;        
-
-% Calculate X and Y Coordinates using already known Z
-Red_X_Camera = (Z_Cam_To_Block*Red_X_L) / focalLength;
-Red_Y_Camera = (Z_Cam_To_Block*Red_Y_L) / focalLength;
-
-Green_X_Camera = (Z_Cam_To_Block*Green_X_L) / focalLength;
-Green_Y_Camera = (Z_Cam_To_Block*Green_Y_L) / focalLength;
-
-Yellow_X_Camera = (Z_Cam_To_Block*Yellow_X_L) / focalLength;
-Yellow_Y_Camera = (Z_Cam_To_Block*Yellow_Y_L) / focalLength;
-
-Blue_X_Camera = (Z_Cam_To_Block*Blue_X_L) / focalLength;
-Blue_Y_Camera = (Z_Cam_To_Block*Blue_Y_L) / focalLength;
-
-Orange_X_Camera = (Z_Cam_To_Block*Orange_X_L) / focalLength;
-Orange_Y_Camera = (Z_Cam_To_Block*Orange_Y_L) / focalLength;
-
-% Store in 1 by 3 Matrix of Coordinates (x,y,z)
-RED_CamCoordinate = [Red_X_Camera Red_Y_Camera Z_Cam_To_Block];
-GREEN_CamCoordinate = [Green_X_Camera Green_Y_Camera Z_Cam_To_Block];
-YELLOW_CamCoordinate = [Yellow_X_Camera Yellow_Y_Camera Z_Cam_To_Block];
-BLUE_CamCoordinate = [Blue_X_Camera Blue_Y_Camera Z_Cam_To_Block];
-ORANGE_CamCoordinate = [Orange_X_Camera Orange_Y_Camera Z_Cam_To_Block];
+% Camera (xyz) coordinates
+rCameraCoordinate = calculateCameraCoordinates(focalLength,PPx,PPy,red_centre,zCameraToBlock);
+gCameraCoordinate = calculateCameraCoordinates(focalLength,PPx,PPy,green_centre,zCameraToBlock);
+yCameraCoordinate = calculateCameraCoordinates(focalLength,PPx,PPy,yellow_centre,zCameraToBlock);
+bCameraCoordinate = calculateCameraCoordinates(focalLength,PPx,PPy,blue_centre,zCameraToBlock);
+oCameraCoordinate = calculateCameraCoordinates(focalLength,PPx,PPy,orange_centre,zCameraToBlock);
 
 %% Convert Camera Coordinates to Dobot Coordinates
     
 % Dobot Parameters
-groundToDobotCentre = 0.057;
-blockHeight = 0.0260;
-tableheight = 0.0900;
+% groundToDobotCentre = 0.057;
+blockHeight = 0.03;
+% tableheight = 0.0900;
     
-% Converting Camera Coordinates to Dobot Coordinates (1x3 Matrix of Coordinates each)
-rDobot = [0.26925-RED_CamCoordinate(1,2) (-1)*RED_CamCoordinate(1,1) 0.08]         
-rDobotHover = [0.26925-RED_CamCoordinate(1,2) (-1)*RED_CamCoordinate(1,1) 0-groundToDobotCentre+blockHeight];
+% Converting Camera Coordinates to Dobot Coordinates (each stored in a 1x3 matrix)
+rDobot = [(0.2575+rCameraCoordinate(1,2)) rCameraCoordinate(1,1) -0.0375]        
+rDobotHover = [(0.2575+rCameraCoordinate(1,2)) rCameraCoordinate(1,1) 0.1];
 
-gDobot = [0.26925-GREEN_CamCoordinate(1,2) (-1)*GREEN_CamCoordinate(1,1) 0.08]
-gDobotHover = [0.26925-GREEN_CamCoordinate(1,2) (-1)*GREEN_CamCoordinate(1,1) 0-groundToDobotCentre+blockHeight];
+gDobot = [(0.2575+gCameraCoordinate(1,2)) gCameraCoordinate(1,1) -0.0375]
+gDobotHover = [(0.2575+gCameraCoordinate(1,2)) gCameraCoordinate(1,1) 0.1];
 
-yDobot = [0.26925-YELLOW_CamCoordinate(1,2) (-1)*YELLOW_CamCoordinate(1,1) 0.08]
-yDobotHover = [0.26925-BLUE_CamCoordinate(1,2) (-1)*BLUE_CamCoordinate(1,1) 0-groundToDobotCentre+blockHeight];
+yDobot = [(0.2575+yCameraCoordinate(1,2)) yCameraCoordinate(1,1) -0.0375]
+yDobotHover = [(0.2575+yCameraCoordinate(1,2)) yCameraCoordinate(1,1) 0.1];
 
-bDobot = [0.26925-BLUE_CamCoordinate(1,2) (-1)*BLUE_CamCoordinate(1,1) 0.08]
-bDobotHover = [0.26925-BLUE_CamCoordinate(1,2) (-1)*BLUE_CamCoordinate(1,1) 0-groundToDobotCentre+blockHeight];
+bDobot = [(0.2575+bCameraCoordinate(1,2)) bCameraCoordinate(1,1) -0.0375]
+bDobotHover = [(0.2575+bCameraCoordinate(1,2)) bCameraCoordinate(1,1) 0.1];
 
-oDobot = [0.26925-ORANGE_CamCoordinate(1,2) (-1)*ORANGE_CamCoordinate(1,1) 0.08]
-oDobotHover = [0.26925-ORANGE_CamCoordinate(1,2) (-1)*ORANGE_CamCoordinate(1,1) 0-groundToDobotCentre+blockHeight];
+oDobot = [(0.2575+oCameraCoordinate(1,2)) oCameraCoordinate(1,1) -0.0375]
+oDobotHover = [(0.2575+oCameraCoordinate(1,2)) oCameraCoordinate(1,1) 0.1];
     
-% Dropoff Coordinates    
-primaryDropoffHover = [0.26925, -0.072, 0.1];
-primaryDropoff1 = [0.26925, -0.072, -0.03];
-primaryDropoff2 = [0.26925, -0.072, -0.03+blockHeight];
-primaryDropoff3 = [0.26925, -0.072, -0.03++(2*blockHeight)];
+%% Dropoff Coordinates (Hardcoded Values)
+
+% For primary colours (Red,Yellow,Blue)
+primaryDropoffHover = [0.2575, -0.072, 0.1];
+primaryDropoff1 = [0.2575, -0.075, -0.03];
+primaryDropoff2 = [0.2575, -0.075, -0.03+blockHeight];
+primaryDropoff3 = [0.2575, -0.075, -0.03+(2*blockHeight)];
  
-secondaryDropoffHover = [0.26925, 0.072, 0.1];
-secondaryDropoff1 = [0.26925, 0.072, -0.03];
-secondaryDropoff2 = [0.26925, 0.072, -0.03+blockHeight];
+% For secondary colours (Orange,Purple)
+secondaryDropoffHover = [0.2575, 0.075, 0.1];
+secondaryDropoff1 = [0.2575, 0.075, -0.03];
+secondaryDropoff2 = [0.2575, 0.075, -0.03+blockHeight];
     
 %% Initalise Dobot
   
@@ -153,73 +120,128 @@ pause(25);          % Long pause as robot needs to be fully initialised before s
 
 fprintf('\nDobot is initialised\n');
 
+%% Priming Motions (Movement)
+
+target = [0.2575,0,0.03];
+
+[targetEndEffectorPub,targetEndEffectorMsg] = rospublisher('/dobot_magician/target_end_effector_pose');
+    
+targetEndEffectorMsg.Position.X = target(1);
+targetEndEffectorMsg.Position.Y = target(2);
+targetEndEffectorMsg.Position.Z = target(3);
+    
+target_rotation = [0,0,0];
+qua = eul2quat(target_rotation);
+targetEndEffectorMsg.Orientation.W = qua(1);
+targetEndEffectorMsg.Orientation.X = qua(2);
+targetEndEffectorMsg.Orientation.Y = qua(3);
+targetEndEffectorMsg.Orientation.Z = qua(4);
+     
+send(targetEndEffectorPub,targetEndEffectorMsg);         % Send command to move
+
+pause(3)
+
+fprintf('\nMovement is primed\n');            % Display end effector current position
+
+%% Priming Motions (Tool State)
+
+state = 0;                        % Binary operator - change to toggle grip
+
+[toolStatePub, toolStateMsg] = rospublisher('/dobot_magician/target_tool_state');
+toolStateMsg.Data = [1 state];
+send(toolStatePub,toolStateMsg);  % Send command to gripper
+
+pause (2)
+
+fprintf('\nSuction is primed\n');            % Display message for user
+
 %% Movement of the Robot
 
 % Moving Red Block
 moveEndEffector(rDobotHover);
 pause(5);
 moveEndEffector(rDobot);
-pause(2);
+pause(3);
 setToolState(1);
 pause(2);
+moveEndEffector(rDobotHover);
+pause(3);
 moveEndEffector(primaryDropoffHover);
-pause(5);
+pause(3);
 moveEndEffector(primaryDropoff1);
-pause(2);
+pause(3);
 setToolState(0);
 pause(2);
+moveEndEffector(primaryDropoffHover);
+pause(3);
 
 % Moving Yellow Block
 moveEndEffector(yDobotHover);
 pause(5);
 moveEndEffector(yDobot);
-pause(2);
+pause(3);
 setToolState(1);
 pause(2);
+moveEndEffector(yDobotHover);
+pause(3);
 moveEndEffector(primaryDropoffHover);
 pause(5);
 moveEndEffector(primaryDropoff2);
-pause(2);
+pause(3);
 setToolState(0);
 pause(2);
+moveEndEffector(primaryDropoffHover);
+pause(3);
 
 % Moving Blue Block
 moveEndEffector(bDobotHover);
 pause(5);
 moveEndEffector(bDobot);
-pause(2);
+pause(3);
 setToolState(1);
 pause(2);
+moveEndEffector(bDobotHover);
+pause(3);
 moveEndEffector(primaryDropoffHover);
 pause(5);
 moveEndEffector(primaryDropoff2);
-pause(2);
+pause(3);
 setToolState(0);
 pause(2);
+moveEndEffector(primaryDropoffHover);
+pause(3);
 
 % Moving Green Block
 moveEndEffector(gDobotHover);
 pause(5);
 moveEndEffector(gDobot);
-pause(2);
+pause(3);
 setToolState(1);
 pause(2);
-moveEndEffector(secondaryaryDropoffHover);
+moveEndEffector(gDobotHover);
+pause(3);
+moveEndEffector(secondaryDropoffHover);
 pause(5);
 moveEndEffector(secondaryDropoff1);
-pause(2);
+pause(3);
 setToolState(0);
 pause(2);
+moveEndEffector(secondaryDropoffHover);
+pause(3);
 
 % Moving Orange Block
 moveEndEffector(oDobotHover);
 pause(5);
 moveEndEffector(oDobot);
-pause(2);
+pause(3);
 setToolState(1);
 pause(2);
-moveEndEffector(secondaryaryDropoffHover);
+moveEndEffector(oDobotHover);
+pause(3);
+moveEndEffector(secondaryDropoffHover);
 pause(5);
 moveEndEffector(secondaryDropoff2);
-pause(2);
+pause(3);
 setToolState(0);
+pause(2);
+moveEndEffector(secondaryDropoffHover);
